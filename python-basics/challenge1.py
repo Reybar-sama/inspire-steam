432a8bb089f975563ca6380c01186371f132e474
Reybar-sama <reyhanbaraka@gmail.com>
2026-02-11 09:40:36 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+grade = "A-"
+favourite_course = "BSc. Mechatronics Engineering"
+cluster_points = "43.214"
+
+print(f"My favourite university is {university}")
+print(f"I scored a KCSE grade of {grade}")
+print(f"My course of choice is {favourite_course}")
+print(f"My cluster points are {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("hello from ReyBar")
+# city / town
+print("I am from Murang'a")
+# my hobbies
+print("I like to play football, hiking, camping, driving and fixing.")
+
+age = 67
+height = 1.84
+weight = 69
+
+print(f"I am {age} years old")
+print(f"I am also {height} metres tall")
+print(f"And weigh {weight} kgs")
