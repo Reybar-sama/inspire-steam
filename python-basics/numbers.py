58a8fbe4b2bb3303965998bb66cd1b8cb505956c
Reybar-sama <reyhanbaraka@gmail.com>
2026-02-11 11:01:15 +0300

    program to add, subtract, divide and multiply 2 numbers from input

---
+++ python-basics/numbers.py
@@ -0,0 +1,19 @@
+# Name : Rey Bar
+# Date : 11/02/2026
+
+# program to add, subtract, divide and multiply 2 numbers from input
+
+first_number = int(input("Enter the first number"))
+second_number = int(input("Enter the second number"))
+
+
+sum_numbers = first_number + second_number
+difference_numbers = first_number - second_number
+quotient_numbers = first_number / second_number
+product_numbers = first_number * second_number
+
+
+print(f"The sum of the numbers : {sum_numbers}")
+print(f"The difference of the numbers : {difference_numbers}")
+print(f"The quotient of the numbers : {quotient_numbers}")
+print(f"The product of the numbers : {product_numbers}")
